ec2eec33ab23ab5470b9f10806a650a233f72970
Ilker Ispir <ilker.ispir@alo-tech.com>
2020-08-19 16:05:30 +0300

    fix: modified main.go

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "github.com/gin-gonic/gin"
 func main() {
 	r := gin.Default()
 
-	r.GET("/ping", func(c *gin.Context) {
+	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"dev": "go",
 			"message": "Hello World!",
